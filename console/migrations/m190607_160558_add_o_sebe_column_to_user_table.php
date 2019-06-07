5dabbd4d9c41c1390b94e9b99ebbe223b739e6c2
pavel <kimpauevg@mail.ru>
2019-06-07 23:17:25 +0700

    Added columns 'o_sebe' and 'phone' to 'user' class

---
+++ console/migrations/m190607_160558_add_o_sebe_column_to_user_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding o_sebe to table `{{%user}}`.
+ */
+class m190607_160558_add_o_sebe_column_to_user_table extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('{{%user}}', 'o_sebe', $this->string()->defaultValue(null));
+        $this->addColumn('{{%user}}', 'phone', $this->string(12)->defaultValue(null));
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+    }
+}
